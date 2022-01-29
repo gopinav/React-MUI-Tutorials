4d302db41acb5f3a28727defb6dffee15c78c0c7
Vishwas <vishwas.g5@gmail.com>
2022-01-29 18:09:03 +0530

    Add Select component

--- react-mui-demo/src/App.tsx
+++ react-mui-demo/src/App.tsx
@@ -1,14 +1,16 @@
 // import { MuiTypography } from './components/MuiTypography'
 // import { MuiButton } from './components/MuiButton'
+// import { MuiTextField } from './components/MuiTextField'
 import './App.css'
-import { MuiTextField } from './components/MuiTextField'
+import { MuiSelect } from './components/MuiSelect'
 
 function App() {
   return (
     <div className='App'>
       {/* <MuiTypography /> */}
       {/* <MuiButton /> */}
-      <MuiTextField />
+      {/* <MuiTextField /> */}
+      <MuiSelect />
     </div>
   )
 }
---
+++ react-mui-demo/src/components/MuiSelect.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { Stack, TextField, MenuItem } from '@mui/material'
+
+export const MuiSelect = () => {
+  const [countries, setCountries] = useState<string[]>([])
+  console.log(countries)
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value
+    setCountries(typeof value === 'string' ? value.split(',') : value)
+  }
+  return (
+    <Stack spacing={4} width='250px'>
+      <TextField
+        label='Select country'
+        select
+        SelectProps={{
+          multiple: true
+        }}
+        size='small'
+        color='secondary'
+        helperText='Please select your country'
+        value={countries}
+        onChange={handleChange}>
+        <MenuItem value='IN'>India</MenuItem>
+        <MenuItem value='US'>USA</MenuItem>
+        <MenuItem value='AU'>Australia</MenuItem>
+      </TextField>
+    </Stack>
+  )
+}
